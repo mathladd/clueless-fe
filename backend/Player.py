5687e9bf6402eefcdb175397075228b9ea7c4450
DK <dakotapalmer@gmail.com>
2023-10-21 22:08:09 -0400

    Saving Work

---
+++ backend/Player.py
@@ -0,0 +1,46 @@
+# Player Class
+# Holds player username, state, can_win, and cards dealt
+
+class Player:
+    # all players should start off as in lobby once signed in
+    def __init__(self, websocket, username, state):
+        # Connection
+        self.websocket = websocket
+        
+        # username for player
+        self.username = username
+
+        # STATES = IN_LOBBY, IN_ROOM, IN_GAME, WAITING_TURN, 
+        #          CURRENT_TURN, SUGGESTING, ACCUSING
+        self.state = state
+
+        # Character assigned in lobby
+        # Allow characters to choose in order or rolled dice.
+        self.character = None
+
+        # Set to False if making accusaion
+        self.can_win = True
+
+        # To be filled in after winning combo chosen
+        self.cards = []
+
+        # Every card is a true possibilty unless marked false by player
+        self.clue_sheet = {
+            "card1" : True,
+            "card2" : True
+        }
+
+    ## Player State functionality
+    # function for changing player state
+
+    ## Accusaion functionality
+    # function for alter can win if accused == false
+
+    ## Player Cards
+    # function for getting player cards
+   
+    ## Clue Sheet
+    # function for getting clue sheet
+
+    # function for altering clue sheet
+    
---
+++ backend/Tile.py
@@ -0,0 +1,24 @@
+# Tiles for gameboard
+
+# A tile can be a room or not a room and just a tild in a hallway
+
+class Tile:
+    def __init__(self, tile_name):
+        # if tile_name == TILE, then not a room
+        self.tile_name = tile_name
+
+        # if tile_name == tile, then players can only hold one object
+        # (hallway is blocked)
+        self.players = []
+
+        # if tile_name != TILE
+        # then it is a room and can hold weapons
+        self.weapons = []        
+
+
+    # function for determining if it is a room or tile
+
+    ## Tile functions
+
+    ## Room functions
+    # function for determining if it has a diagonal room
