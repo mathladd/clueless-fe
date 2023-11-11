33ec6b9ff13f729bfe9d202c19d480d529a86a01
Duy Nguyen <duynghoang05@gmail.com>
2023-11-11 07:40:13 +0700

    fix hydration error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GameSession from 'components/GameSession';
 import { WS } from 'types/common';
 import LobbyScreen from 'components/LobbyScreen';
 
 function HomePage({ ws }: { ws: WS }) {
   const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
+  const [isMounted, setIsMounted] = useState(false);
 
+  useEffect(() => setIsMounted(true), []);
+  if (!isMounted) return null;
   return (
     <>
       {isGameStarted || true ? (
